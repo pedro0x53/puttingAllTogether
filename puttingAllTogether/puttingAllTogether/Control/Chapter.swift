c089e6792beab4e93ebac3b8cbd568ff9d9b86ea
pthesea <pedrosousa2708@gmail.com>
2020-06-03 14:55:42 -0300

    Gameplay view and controller created; Gestures recognizer added to Gameplay view; Initial (View and Control) and Config files updated to agree with MVC architecture

---
+++ puttingAllTogether/puttingAllTogether/Control/Chapter.swift
@@ -0,0 +1,31 @@
+//
+//  Chapter.swift
+//  puttingAllTogether
+//
+//  Created by Hiago Chagas on 15/06/20.
+//  Copyright © 2020 Brabo. All rights reserved.
+//
+
+import Foundation
+
+struct Chapter{
+    let chapterID: Int
+    let scenes: [Scene]
+    
+//    func playScenes(){
+//        for scene in scenes{
+//            //play the audio
+//            if(scene.hasSFX == true){
+//                //play the SFX
+//            }
+//            if(scene.hasGestures == true){
+//                //put the gesture on screen
+//            }
+//            //check for
+//
+//        }
+//    }
+    func nextChapter(){
+        print("Goes into the next chapter - insert code here")
+    }
+}
---
+++ puttingAllTogether/puttingAllTogether/Control/Scene.swift
@@ -0,0 +1,17 @@
+//
+//  Scene.swift
+//  puttingAllTogether
+//
+//  Created by Hiago Chagas on 15/06/20.
+//  Copyright © 2020 Brabo. All rights reserved.
+//
+
+import Foundation
+
+struct Scene{
+    let audioURL: String
+    let hasGestures: Bool
+    let gestures: [String]?
+    let hasSFX: Bool
+    let sfx: [String]?
+}
